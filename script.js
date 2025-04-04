2bb1bee3c763259a7f2d0e34aac8df9f64a349eb
CodeWithMatlhogonolo <Matlhogonolo065@gmail.com>
2025-04-05 01:16:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thanks for your message! We'll get back to you soon.");
+  this.reset();
+});
